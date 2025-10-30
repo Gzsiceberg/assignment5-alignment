87b135c8e8a777c8e9c4d497ed83a8fd3394d785
gzs_iceberg <gzs_iceberg@outlook.com>
2025-10-30 22:08:59 +0800

    add black as a dependency and create download_models script

---
+++ scripts/download_models.py
@@ -0,0 +1,13 @@
+from huggingface_hub import snapshot_download
+
+
+if __name__ == "__main__":
+    models = ["Qwen/Qwen2.5-Math-1.5B"]
+    for model in models:
+        print(f"Downloading {model}...")
+        snapshot_download(
+            model,
+            local_dir=f"./models/{model}",
+            resume_download=True,
+            local_dir_use_symlinks=False
+        )
