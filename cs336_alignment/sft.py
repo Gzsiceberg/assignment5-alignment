98b4c4392fdcebdb5ab1f6b89e65b40627776fc5
Iceberg <gzs_iceberg@outlook.com>
2025-11-04 17:49:02 +0800

    Refactor evaluation interval calculation and logging in main execution block

--- cs336_alignment/sft.py
+++ cs336_alignment/sft.py
@@ -142,6 +142,8 @@ if __name__ == "__main__":
     batch_size = sft_config.micro_batch_size * sft_config.gradient_accumulation_steps
     training_steps = sft_config.num_epochs * example_count // batch_size
     print(f"Total training steps: {training_steps} batch size: {batch_size} example count: {example_count}")
+    eval_interval = sft_config.eval_interval * example_count // batch_size if sft_config.eval_interval > 0 else 0
+    print_and_log(f"Evaluation interval (in steps): {eval_interval}")
 
     from vllm.sampling_params import SamplingParams
     from math_baseline import generate_prompt_and_gt, evaluate_vllm
@@ -206,8 +208,6 @@ if __name__ == "__main__":
         training_steps
     )
 
-    eval_interval = sft_config.eval_interval * sample_count // batch_size if sft_config.eval_interval > 0 else 0
-    print_and_log(f"Evaluation interval (in steps): {eval_interval}")
     for st in (pbar := trange(training_steps, desc="SFT Training Steps")):
         for _ in trange(gradient_accumulation_steps, desc="Gradient Accumulation Steps", leave=False):
             random_index = np.random.randint(0, sample_count, size=batch_size)
@@ -232,7 +232,7 @@ if __name__ == "__main__":
         pbar.set_description(f"Loss: {loss.item():.4f} avg_token_entropy: {token_entropy.mean().item():.4f} lr: {current_lr:.6f}") # type: ignore
         
         is_last_step = st == training_steps - 1
-        if vllm_model and eval_interval > 0 and ((st + 1) % eval_interval == 0 or is_last_step):
+        if vllm_model is not None and ((st + 1) % eval_interval == 0 or is_last_step):
             print_and_log(f"Running evaluation at step {st+1}...")
             load_policy_into_vllm_instance(llm, vllm_model) # # type: ignore
             evaluate_vllm(
