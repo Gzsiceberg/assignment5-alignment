80faff1751c90e8dea28c6fef9fe26bf20de4daa
Iceberg <gzs_iceberg@outlook.com>
2025-11-10 13:18:19 +0800

    Update import statement and fix policy loading in training logic

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -20,7 +20,7 @@ from cs336_alignment.sft import cleanup, do_grad_accumulate
 from cs336_alignment.sft_helper import masked_normalize, masked_mean, tokenize_to_tensor
 from cs336_alignment.logger import print_and_log, setup_logging
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel  # type: ignore
-from cs336_alignment.vllm_util import init_vllm  # type: ignore
+from cs336_alignment.vllm_util import init_vllm, load_policy_into_vllm_instance
 from cs336_alignment.config import load_config_from_file
 from einops import rearrange, einsum
 from cs336_alignment.sft_helper import get_response_log_probs
@@ -420,7 +420,7 @@ def train(config_name: str = typer.Argument("config/grpo_test.yaml")):
             )
             if llm is not None:
                 print_and_log("Loading policy into vLLM instance...")
-                load_policy_into_vllm_instance(llm, vllm_model)  # # type: ignore
+                load_policy_into_vllm_instance(llm, vllm)  # # type: ignore
 
         if rl_config.eval_interval > 0 and ((step + 1) % rl_config.eval_interval == 0 or is_last_step):
             evaluate_vllm(vllm, eval_prompts, eval_ground_truths, eval_sampling_params)
